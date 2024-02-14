046fece03569e66baa21b350ad41e76c0d50b7c1
mnmnkm <152707508+mnmnkm@users.noreply.github.com>
2024-02-14 14:36:48 +0000

    [Update] sign_up,sign_in,sign_outのフラッシュメッセージ追加中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -24,19 +24,32 @@ class BooksController < ApplicationController
   
   def edit
     @book = Book.find(params[:id])
+    unless @book.user == current_user
+      redirect_to book_path
+    end
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    flash[:notice] = "You have updated book successfully."
-    redirect_to book_path(book.id )
+    @book = Book.find(params[:id])
+    if @book.user != current_user
+      redirect_to book_path(book.id)
+    else
+      if @book.update(book_params)
+        flash[:notice] = "You have updated book successfully."
+        redirect_to book_path(book.id)
+      else
+        render :show
+      end
+    end
   end
   
   def destroy
-    book = Book.find(params[:id])
-    book.destroy
-    redirect_to books_path
+    @book = Book.find(params[:id])
+    if @book.user != current_user
+      redirect_to book_path
+    else
+      @book.destroy
+    end
   end
   
   private
